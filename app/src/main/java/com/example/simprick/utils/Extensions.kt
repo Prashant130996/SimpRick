cf0b464fd5d1acab48492a51a48fd377d443ec3b
Nehra-Agicent <prashant.nehra@agicent.in>
2023-07-27 17:40:24 +0530

    added Extensions fun for showing toast

--- app/src/main/java/com/example/simprick/utils/Extensions.kt
+++ app/src/main/java/com/example/simprick/utils/Extensions.kt
@@ -1,5 +1,6 @@
 package com.example.simprick.utils
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Build
@@ -7,10 +8,24 @@ import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
 import android.os.Parcelable
 import android.widget.Toast
+import androidx.annotation.StringRes
+import androidx.fragment.app.Fragment
 import java.io.Serializable
 
-fun Context.showToast(msg: String) {
-    Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+fun Fragment.toast(message: String) {
+    Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
+}
+
+fun Fragment.toast(@StringRes message: Int) {
+    Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
+}
+
+fun Activity.toast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
+
+fun Activity.toast(@StringRes message: Int) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
 }
 
 inline fun <reified T : Parcelable> Intent.parcelable(key: String): T? = when {
@@ -39,6 +54,6 @@ inline fun <reified T : Serializable> Bundle.serializable(key: String): T? = whe
 }
 
 inline fun <reified T : Serializable> Intent.serializable(key: String): T? = when {
-    Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getSerializableExtra(key, T::class.java)
+    SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getSerializableExtra(key, T::class.java)
     else -> @Suppress("DEPRECATION") getSerializableExtra(key) as? T
 }
